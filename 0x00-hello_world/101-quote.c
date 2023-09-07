e8f6175d108d35c0e0669c9061fd939d1894051f
Madani-Bakhet <madani2077@gmail.com>
2023-09-07 22:03:44 +0200

    extra

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+    /* Use write to print to standard error (file descriptor 2) */
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+
+    return (1);
+}
