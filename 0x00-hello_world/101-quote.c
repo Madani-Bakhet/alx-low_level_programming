54d0df4fbe39df0e8a2961bb870dbfd10d440e44
Madani-Bakhet <madani2077@gmail.com>
2023-09-07 22:17:17 +0200

    extra

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
  * main - Entry point of the program
  *
@@ -9,8 +6,6 @@
  */
 int main(void)
 {
-    /* Use write to print to standard error (file descriptor 2) */
-   /*write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));*/
-    puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-    return (1);
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
 }
