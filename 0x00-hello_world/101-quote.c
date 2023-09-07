b0a8c470556ba7ffa5a75f61b25e35be32b927f3
Madani-Bakhet <madani2077@gmail.com>
2023-09-07 22:05:17 +0200

    extra

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
     /* Use write to print to standard error (file descriptor 2) */
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+    fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 
     return (1);
 }
