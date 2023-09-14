b559af14ab66a0bc105a7d9f6b36346f633aacef
Madani-Bakhet <madani2077@gmail.com>
2023-09-14 22:53:44 +0200

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,10 +2,10 @@
 
 /**
   * main - prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz
-  * instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both
-  * three and five print FizzBuzz.
+  *        instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both
+  *        three and five print FizzBuzz.
   *
-  * Return : always 0.
+  * Return: always 0.
   */
 int main(void)
 {
@@ -24,6 +24,5 @@ int main(void)
 		printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
