46648c851b2271cc4629ad5d27946c5fafac116a
Madani-Bakhet <madani2077@gmail.com>
2023-09-14 14:05:47 +0200

    4, 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
-  * print_numbers - print nums from 0 - 9
+  * print_most_numbers - print nums from 0 - 9
   */
 void print_most_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-if (i == 2 || i == 4)
-{
-continue;
-}
-else
-{
-_putchar(i +'0');
-}
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i +'0');
+		}
+	}
+	_putchar('\n');
 }
