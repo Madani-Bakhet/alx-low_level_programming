925eddb1a516942a70c18e78e0d54dfcfc76ba41
Madani-Bakhet <madani2077@gmail.com>
2023-09-14 22:43:36 +0200

    the remaining

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,8 @@
 
 /**
   * main - FizzBuzz
+  *
+  * return : always 0.
   */
 int main(void)
 {
@@ -15,6 +17,8 @@ int main(void)
 			printf("Buzz");
 		if (i % 3 != 0 && i % 5 != 0)
 			printf("%d", i);
+		if (i == 100)
+			continue;
 		printf(" ");
 	}
 	printf("\n");
