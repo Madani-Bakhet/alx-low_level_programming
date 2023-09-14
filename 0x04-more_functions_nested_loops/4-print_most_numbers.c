237caa920f7bc9993f9da025353f081c2935b2ed
Madani-Bakhet <madani2077@gmail.com>
2023-09-14 10:28:07 +0200

    4, 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,16 @@
 /**
   * print_numbers - print nums from 0 - 9
   */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i +'0');
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - print all alphabet 10 times
+  */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j  = 0; j <= 14; j++)
+			if (j > 9)
+			{
+				_putchar('1');
+				_putchar(j % 10 +'0');
+			}
+			else
+				_putchar(j + '0');
+		_putchar('\n');
+	}
+}
