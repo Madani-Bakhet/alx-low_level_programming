9f977104b6b618bd621a34c71cefde3c9b757887
Madani-Bakhet <madani2077@gmail.com>
2023-09-14 14:04:12 +0200

    4, 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,18 +5,17 @@
   */
 void print_most_numbers(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		if (i == 2 || i == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(i +'0');
-		}
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+{
+continue;
+}
+else
+{
+_putchar(i +'0');
+}
+}
+_putchar('\n');
 }
