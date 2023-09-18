cd9bf75d0f6a4b8872149c5d541d0f017fbc0f7c
Madani-Bakhet <madani2077@gmail.com>
2023-09-18 21:29:37 +0200

    5 - 7

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,8 @@ void rev_string(char *s)
 	int i, j;
 	char temp;
 
-	for (i = 0, j = length - 1; i < j; i++, j--) {
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
 		temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
