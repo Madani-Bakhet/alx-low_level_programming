32af1d9bade20c1e830f2c5849f432c9a03f3602
Madani-Bakhet <madani2077@gmail.com>
2023-09-21 21:26:03 +0200

    extra

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * rot13 - encode to rot13
+  * @str: string
+  *
+  * Return: encoded string
+  */
+int main(void)
+{
+	char a = 'a' + 1;
+	printf("%c",a);
+	return (0);
+}
