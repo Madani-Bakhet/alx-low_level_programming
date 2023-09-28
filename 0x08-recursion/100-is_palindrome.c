576b4ce3578fb1a33ce431545535618a5949fdd7
Madani-Bakhet <madani2077@gmail.com>
2023-09-28 13:49:33 +0200

    extra

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,7 +16,7 @@ int is_palindrome_helper(char *s, int start, int end)
 	if (s[start] != s[end])
 		return (0);
 
-	return is_palindrome_helper(s, start + 1, end - 1);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
