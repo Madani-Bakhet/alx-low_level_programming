3cfb30b7cc5086f26e161f7c5b945c73f9e5ee02
Madani-Bakhet <madani2077@gmail.com>
2023-10-02 13:43:59 +0200

    0x0A argc, extra

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main
+  * @argc: count
+  * @argv: array
+  *
+  *Return: always 0
+  */
+int main(int argc, char **argv)
+{
+	int sum = 0, remin;
+
+	if (argc == 2)
+	{
+		remin = atoi(*++argv);
+		sum += remin / 25;
+		remin = remin % 25;
+		sum += remin / 10;
+		remin = remin % 10;
+		sum += remin / 5;
+		remin = remin % 5;
+		sum += remin / 2;
+		remin = remin % 2;
+		sum += remin / 1;
+		printf("%d", sum);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
