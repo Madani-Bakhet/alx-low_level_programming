2a175002dda48b2a599291869741faac5adac401
Madani-Bakhet <madani2077@gmail.com>
2023-10-04 18:35:18 +0200

    0x0B malloc 0 - 100

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
-  * create_array - create array
-  * @size: size
-  * @c: char
-  *
-  * Return: pointer
-  */
+ * create_array - Creates an array of chars and
+ *                initializes it with a specific char.
+ * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
+ *
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *ar;
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	ar = malloc(size * sizeof(int));
-	for (i = 0; i < size; i++)
-	{
-		ar[i] = c;
-	}
-	return (ar);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,36 +1,46 @@
 #include "main.h"
 
 /**
-  * allocate_grid - print grid
-  * @h: height
-  * @w: width
-  *
-  * Return: pointer to 2 dim array
-  */
-int **alloc_grid(int w, int h)
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
+ */
+int **alloc_grid(int width, int height)
 {
-	int **grid, i, j;
+	int **twoD;
+	int hgt_index, wid_index;
 
-	if (w == 0 || h == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(sizeof(int *) * h);
-	if (grid == NULL)
+
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
 		return (NULL);
-	for (i = 0; i < h; i++)
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		grid[i] = malloc(sizeof(int) * w);
-		if (grid[i] == NULL)
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
 		{
-			for (;i > 0;i--)
-				free(grid[i]);
-			free(grid);
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
 			return (NULL);
 		}
 	}
-	for (i = 0; i < h; i++)
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		for (j = 0; j < w; j++)
-			grid[h][w] = 0;
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
 	}
-	return (grid);
+
+	return (twoD);
 }
